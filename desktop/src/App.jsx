e82c2665c44db2acc319c9335bc857e67dd14816
devashree pathak <devashreepathak22@gmail.com>
2026-08-09 12:57:06 +0530

    Set up Electron + React desktop pet foundation

---
+++ desktop/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div
+      className="w-screen h-screen flex items-center justify-center bg-transparent"
+      style={{ WebkitAppRegion: "drag" }}
+    >
+      <div className="text-8xl select-none">🐱</div>
+    </div>
+  );
+}
