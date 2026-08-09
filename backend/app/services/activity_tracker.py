8b377d43ab770fed00c88a1b807c5539cb976a41
devashree pathak <devashreepathak22@gmail.com>
2026-08-09 11:35:43 +0530

    Add persistent activity logging and activity classification system

--- backend/app/services/activity_tracker.py
+++ backend/app/services/activity_tracker.py
@@ -1,6 +1,7 @@
 import time
 import win32gui
-
+from datetime import datetime
+from app.utils.activity_logger import log_activity
 from app.utils.activity_classifier import classify_activity
 
 def get_active_window():
@@ -36,9 +37,20 @@ if __name__ == "__main__":
                     previous_time = current_time
                     continue
 
+
+                record = {
+                "start_time": datetime.fromtimestamp(previous_time).strftime("%Y-%m-%d %H:%M:%S"),
+                "end_time": datetime.fromtimestamp(current_time).strftime("%Y-%m-%d %H:%M:%S"),
+                "duration_seconds": round(duration, 1),
+                "category": category,
+                "window_title": previous_window
+                }
+
+                log_activity(record)
+
                 print(
                     f"{category:15} | {duration:6.1f}s | {previous_window}"
-                )
+        )
 
             previous_window = active_window
             previous_time = current_time
